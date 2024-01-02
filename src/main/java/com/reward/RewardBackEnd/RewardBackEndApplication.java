b7eb2f9d7e56f198909fc49d1161a324be8add55
utpalpatel0055 <utpalpatel0055@gmail.com>
2024-01-02 11:47:48 -0500

    Initial Project
    Init prep

---
+++ src/main/java/com/reward/RewardBackEnd/RewardBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.reward.RewardBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RewardBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RewardBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reward/RewardBackEnd/RewardBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reward.RewardBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RewardBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
