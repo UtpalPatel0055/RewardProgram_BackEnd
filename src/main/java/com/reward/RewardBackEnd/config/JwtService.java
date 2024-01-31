f450b498d6b27dde02b169712bd0c1843d41862e
Dharti Patel <dhartipatel3109@gmail.com>
2024-01-31 17:42:49 -0500

    JwtService and JwtAuthenticationFilter implemented

--- src/main/java/com/reward/RewardBackEnd/config/JwtService.java
+++ src/main/java/com/reward/RewardBackEnd/config/JwtService.java
@@ -1,12 +1,69 @@
 package com.reward.RewardBackEnd.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
     
+    private final String PD_PATEL = "P8QR2Wbo3Rw/smL7aoxCHflGTiEnGVqyT2Teki4QsHw=";
 
     public String extractUserEmail(String token) {
-        return null;
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSigningKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(PD_PATEL);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimResolver.apply(claims);
+    }
+
+    public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
+        return Jwts.builder()
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24 * 30))
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256).compact();
+    }
+
+    public String generateToken(UserDetails userDetails) {
+        return generateToken(new HashMap<>(), userDetails);
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String userEmail = extractUserEmail(token);
+        return (userEmail.equals(userDetails.getUsername())) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
     }
 }
--- src/main/java/com/reward/RewardBackEnd/controller/StoreController.java
+++ src/main/java/com/reward/RewardBackEnd/controller/StoreController.java
@@ -1,16 +1,14 @@
 package com.reward.RewardBackEnd.controller;
 
-import com.reward.RewardBackEnd.exception.APIErrorDetail;
-import com.reward.RewardBackEnd.exception.APIExceptionHandler;
-import com.reward.RewardBackEnd.exception.save.DuplicateRecordFoundException;
 import com.reward.RewardBackEnd.model.Store;
 import com.reward.RewardBackEnd.service.StoreService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.time.LocalDateTime;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/stores")
@@ -20,12 +18,13 @@ public class StoreController {
     private StoreService storeService;
 
     @PostMapping("/create-store-profile")
-	public ResponseEntity<Store> createNewStore(@RequestBody Store store) {
+    public ResponseEntity<Store> createNewStore(@RequestBody Store store) {
         try {
             Store createdStore = storeService.saveStore(store);
             return new ResponseEntity<Store>(createdStore, HttpStatus.CREATED);
-        } catch(Exception e) {
+        } catch (Exception e) {
             throw e;
         }
     }
+
 }
